8034249bf7c829375b88c26077ffbbc162fb499b
Uthf <uthfvbyrby@gmail.com>
2020-04-28 23:11:27 +0300

    fixes

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -40,6 +42,8 @@ dic_station = {
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
 
 app.layout = html.Div([
 
--- wsgi.py
+++ wsgi.py
@@ -8,5 +8,5 @@ project_home = u'/home/Uthf/DashBoard_RSV'
 if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 
-from main import app
+from app import app
 application = app.server
